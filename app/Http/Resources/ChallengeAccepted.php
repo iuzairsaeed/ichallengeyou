97b4c240d699b6d4da6bf405038774e8e30ee8b2
Uzair Saeed <uzair.saeed27@gmail.com>
2020-06-11 04:31:10 -0700

    change start_time format

--- app/Http/Resources/ChallengeAccepted.php
+++ app/Http/Resources/ChallengeAccepted.php
@@ -17,7 +17,7 @@ class ChallengeAccepted extends JsonResource
         return  [
             'id' => $this->challenge->id,
             'title' => $this->challenge->title,
-            'start_time' => $this->challenge->start_time->format('d M, Y - h:mA'),
+            'start_time' => $this->challenge->start_time->format('d M, Y'),
             'file' => $this->challenge->file,
             'file_mime' => $this->challenge->file_mime,
             'status' => $this->status,
--- app/Http/Resources/ChallengeDonated.php
+++ app/Http/Resources/ChallengeDonated.php
@@ -17,7 +17,7 @@ class ChallengeDonated extends JsonResource
         return  [
             'id' => $this->challenge->id,
             'title' => $this->challenge->title,
-            'start_time' => $this->challenge->start_time->format('d M, Y - h:mA'),
+            'start_time' => $this->challenge->start_time->format('d M, Y'),
             'file' => $this->challenge->file,
             'file_mime' => $this->challenge->file_mime,
             'sum' => $this->sum,
