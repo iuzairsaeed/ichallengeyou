91f7e41634be9fe9e62bc92e6d5bb99937c8767e
aaghouri <ahmedworkid@gmail.com>
2020-06-15 17:10:27 +0500

    FCM corrected

--- app/Notifications/AccountActivated.php
+++ app/Notifications/AccountActivated.php
@@ -6,13 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-use NotificationChannels\Fcm\FcmChannel;
-use NotificationChannels\Fcm\FcmMessage;
-use NotificationChannels\Fcm\Resources\AndroidConfig;
-use NotificationChannels\Fcm\Resources\AndroidFcmOptions;
-use NotificationChannels\Fcm\Resources\AndroidNotification;
-use NotificationChannels\Fcm\Resources\ApnsConfig;
-use NotificationChannels\Fcm\Resources\ApnsFcmOptions;
+use Benwilkins\FCM\FcmMessage;
 
 class AccountActivated extends Notification
 {
@@ -36,7 +30,7 @@ class AccountActivated extends Notification
      */
     public function via($notifiable)
     {
-        return [FcmChannel::class];
+        return ['fcm'];
         // return ['mail'];
     }
 
@@ -48,19 +42,18 @@ class AccountActivated extends Notification
      */
     public function toFcm($notifiable)
     {
-        return FcmMessage::create()
-            ->setData(['data1' => 'value', 'data2' => 'value2'])
-            ->setNotification(\NotificationChannels\Fcm\Resources\Notification::create()
-                ->setTitle('Account Activated')
-                ->setBody('Your account has been activated.')
-                ->setImage('http://example.com/url-to-image-here.png'))
-            ->setAndroid(
-                AndroidConfig::create()
-                    ->setFcmOptions(AndroidFcmOptions::create()->setAnalyticsLabel('analytics'))
-                    ->setNotification(AndroidNotification::create()->setColor('#0A0A0A'))
-            )->setApns(
-                ApnsConfig::create()
-                    ->setFcmOptions(ApnsFcmOptions::create()->setAnalyticsLabel('analytics_ios')));
+        $message = new FcmMessage();
+        $message->content([
+            'title'        => 'Foo',
+            'body'         => 'Bar',
+            'sound'        => '', // Optional
+            'icon'         => '', // Optional
+            'click_action' => '' // Optional
+        ])->data([
+            'param1' => 'baz' // Optional
+        ])->priority(FcmMessage::PRIORITY_HIGH); // Optional - Default is 'normal'.
+
+        return $message;
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        Benwilkins\FCM\FcmNotificationServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
