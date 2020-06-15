537dea6d2c36258e3862eb55ba1e818bdcf8835b
aaghouri <ahmedworkid@gmail.com>
2020-06-15 18:09:30 +0500

    Challenges validation

--- app/Http/Requests/Challenges/ChallengeRequest.php
+++ app/Http/Requests/Challenges/ChallengeRequest.php
@@ -25,13 +25,13 @@ class ChallengeRequest extends FormRequest
     public function rules()
     {
         $rules = [
-            'title' => ['bail', 'required', 'string', 'max:255', 'min:3'],
+            'title' => ['bail', 'required', 'unique:challenges', 'string', 'max:75', 'min:3'],
             'description' => ['bail', 'required', 'max:500', 'min:200'],
             'start_time' => ['required', 'date_format:Y-m-d H:i', 'after:'.date(DATE_ATOM, time() + (5 * 60 * 60))],
             'duration_days' => ['required', 'integer', 'min:0'],
             'duration_hours' => ['required', 'integer', 'min:0', 'max:23'],
             'duration_minutes' => ['required', 'integer', 'min:0', 'max:59'],
-            'location' => ['required', 'string'],
+            'location' => ['required', 'string', 'max:75'],
             'amount' => ['required', 'numeric', 'min:1'],
         ];
         switch ($this->method()) {
@@ -56,6 +56,7 @@ class ChallengeRequest extends FormRequest
     public function messages()
     {
         return [
+            'title.unique' => 'Challenge with the provided title already exists.',
             'start_time.after' => 'Challenge start time must be greater than the current time.'
         ];
     }
