89d1eb8bb8ad5d30d4d30a455646f03985b93f2c
aaghouri <ahmedworkid@gmail.com>
2020-09-24 18:49:07 +0500

    like unlike count

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -5,6 +5,31 @@ function get_setting_option($field)
     return optional(DB::table('settings')->where('field', $field)->first())->value;
 }
 
+function format_number_in_k_notation(int $number): string
+{
+    $suffixByNumber = function () use ($number) {
+        if ($number < 1000) {
+            return sprintf('%d', $number);
+        }
+
+        if ($number < 1000000) {
+            return sprintf('%d%s', floor($number / 1000), 'K+');
+        }
+
+        if ($number >= 1000000 && $number < 1000000000) {
+            return sprintf('%d%s', floor($number / 1000000), 'M+');
+        }
+
+        if ($number >= 1000000000 && $number < 1000000000000) {
+            return sprintf('%d%s', floor($number / 1000000000), 'B+');
+        }
+
+        return sprintf('%d%s', floor($number / 1000000000000), 'T+');
+    };
+
+    return $suffixByNumber();
+}
+
 function time_elapsed_string($datetime, $full = false) {
     $now = new DateTime;
     $ago = new DateTime($datetime);
